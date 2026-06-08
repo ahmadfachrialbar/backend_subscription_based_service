25af1458e8c3387df46d01377ce84fc1742ab5a2
Ahmad Fachri Albar <ahmadfachri846@gmail.com>
2026-06-08 23:15:06 +0700

    Fix production flag for Midtrans Snap initialization

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -5,7 +5,7 @@ const midtransClient = require('midtrans-client');
 
 // Inisialisasi Midtrans Snap client
 const snap = new midtransClient.Snap({
-    isProduction: process.env.MIDTRANS_IS_PRODUCTION === 'false',
+    isProduction: process.env.MIDTRANS_IS_PRODUCTION === 'true',
     serverKey: process.env.MIDTRANS_SERVER_KEY,
     clientKey: process.env.MIDTRANS_CLIENT_KEY
 });
